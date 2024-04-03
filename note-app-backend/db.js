7a03ec1443ceae6de786522d9f546c9fdb2b95eb
barinder <barindersingh1997@gmail.com>
2024-04-03 15:57:55 +0530

    try

--- note-app-backend/db.js
+++ note-app-backend/db.js
@@ -1,8 +1,24 @@
-    const mongoose = require("mongoose")
-    require("dotenv").config()
-    const connection = mongoose.connect(process.env.mongoUrl)
+    // const mongoose = require("mongoose")
+    // require("dotenv").config()
+    // const connection = mongoose.connect(process.env.mongoUrl)
 
-    module.exports = {
-        connection,
+    // module.exports = {
+    //     connection,
         
-    }
+    // }
+
+    const mongoose = require("mongoose");
+
+// Load environment variables from .env file
+require("dotenv").config();
+
+// MongoDB connection URI
+const mongoURI = "mongodb+srv://barindersingh1997:11223344@cluster0.zzxtw7f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Connect to MongoDB
+const connection = mongoose.connect(mongoURI);
+
+// Export the connection
+module.exports = {
+    connection
+};
--- note-app-backend/index.js
+++ note-app-backend/index.js
@@ -1,10 +1,10 @@
 const express = require('express')
-const port = process.env.PORT
-require("dotenv").config()
 const cors = require('cors')
 const {connection} = require("./db")
 const { userRouter } = require('./routes/user.routes')
 const { noteRouter } = require('./routes/note.routes')
+require("dotenv").config()
+const port = process.env.PORT
 const app = express()
 app.use(cors())
 app.use(express.json()) // Middle
