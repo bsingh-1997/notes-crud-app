4b19f3d2f9e47cc1e357b045507c0bbb3a875878
barinder <barindersingh1997@gmail.com>
2024-04-03 15:47:43 +0530

    non

--- note-app-backend/index.js
+++ note-app-backend/index.js
@@ -1,10 +1,10 @@
 const express = require('express')
+const port = process.env.PORT
+require("dotenv").config()
 const cors = require('cors')
 const {connection} = require("./db")
 const { userRouter } = require('./routes/user.routes')
 const { noteRouter } = require('./routes/note.routes')
-require("dotenv").config()
-const port = process.env.PORT
 const app = express()
 app.use(cors())
 app.use(express.json()) // Middle
@@ -26,4 +26,4 @@ app.listen(port,async()=>{
     console.log(`Server is running on ${port}`)
 })
 
-module.exports = app
+module.exports =app
